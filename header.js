2c44fede5ceb0980c85d06eddc9ee17d84e9b452
Etienne Burdet <etienne.burdet@gmail.com>
2020-06-25 17:50:43 +0200

    add js file for inserting header links

---
+++ header.js
@@ -0,0 +1,6 @@
+document.querySelector('.header-left')
+  .insertAdjacentHTML('beforeend',`
+  <a href="#" class="header-link">All courses</a>
+  <a href="#" class="header-link">Thematic courses</a>
+  <a href="#" class="header-link">Knowledge library</a>
+`)
