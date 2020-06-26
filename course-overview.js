cfaf298055e01a174d0e21ca1493f2b624853c17
Etienne Burdet <etienne.burdet@gmail.com>
2020-06-26 18:42:14 +0200

    aligned title

--- course-overview.js
+++ course-overview.js
@@ -1,2 +1,8 @@
 Array.from(document.querySelectorAll('section'))
   .forEach((section) => section.classList.add('active'))
+
+document.querySelector('.sj-summary')
+  .insertAdjacentHTML(
+    'beforebegin',
+    '<img src="https://etienneburdet.com/ods-academy/empty-datasets_yellow.svg" class="course-image">'
+  )
