d81ae4e7f4d2925cea2de64cc355162648256a93
Etienne Burdet <etienne.burdet@gmail.com>
2020-06-29 18:48:39 +0200

    align box icon with flexbox

--- course-boxes.js
+++ course-boxes.js
@@ -1,3 +1,22 @@
+const removeBoxClasses = (element) => {
+  const classes = Array.from(element.classList)
+  classes.forEach(boxClass => {
+    if (boxClass.match(/^course-box.*/)) {
+      element.classList.remove(boxClass)
+    }
+  })
+}
+
+const addIconToBox = (box, icon) => {
+  console.log(box)
+  const boxContent = box.cloneNode()
+  console.log(boxContent)
+  removeBoxClasses(boxContent)
+  box.appendChild(boxContent)
+  const iconTemplate = `<i class="fa fa-${icon} box-icon" aria-hidden="true"></i>`
+  box.insertAdjacentHTML('afterbegin', iconTemplate)
+  box.classList.add('d-flex', 'align-top')
+}
+
 const boxInfo = Array.from(document.querySelectorAll('.course-box-info'))
-const externalLinkTemplate = '<i class="fa fa-external-link box-icon" aria-hidden="true"></i>'
-boxInfo.forEach((box) => box.insertAdjacentHTML('afterbegin', externalLinkTemplate))
+if (boxInfo) { boxInfo.forEach((box) => addIconToBox(box, 'external-link')) }
