c89a249ab774fc4e0424ece2a0d148b3b4862015
Etienne Burdet <etienne.burdet@gmail.com>
2020-06-30 16:28:29 +0200

    backed up sj scripts

---
+++ header-footer.js
@@ -0,0 +1,10 @@
+document.querySelector('.header-left')
+  .insertAdjacentHTML('beforeend',`
+  <a href="#" class="header-link">All courses</a>
+  <a href="#" class="header-link">Thematic courses</a>
+  <a href="#" class="header-link">Knowledge library</a>
+`)
+
+//sj scripts
+$('#ep-footer').append('<div class=footer-content> <a href="https://legal.opendatasoft.com/fr/privacy-policy.html" target="_blank">Politique de confidentialité</a></div>');
+$('#ep-footer').append('<div class=footer-content> <a href="https://legal.opendatasoft.com/fr/cookies.html" target="_blank">Cookies</div>');
