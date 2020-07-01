d430d2454f6c255dd3aa703d6744dda9b7a0b54f
Etienne Burdet <etienne.burdet@gmail.com>
2020-07-01 10:06:47 +0200

    add gitignore and remove font family settings for global

--- sj-scripts.js
+++ sj-scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    var refGuides = document.querySelectorAll('#skilljar-content #catalog-content #catalog-courses a[data-tags*="auto-register"]')
-    refGuides.forEach(function (node) {
-        var href = node.href;
-        if (!~href.indexOf('?reg=1')) {
-            node.href = href + '?reg=1';
-        }
-    })
+  var refGuides = document.querySelectorAll('#skilljar-content #catalog-content #catalog-courses a[data-tags*="auto-register"]')
+  refGuides.forEach(function (node) {
+    var href = node.href;
+    if (!~href.indexOf('?reg=1')) {
+      node.href = href + '?reg=1';
+    }
+  })
 })
