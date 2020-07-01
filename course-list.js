78e53a1636c2011e86e2f6f5c7fda923f219187b
Etienne Burdet <etienne.burdet@gmail.com>
2020-07-01 14:37:31 +0200

    filter menu styling

---
+++ course-list.js
@@ -0,0 +1,12 @@
+document.getElementById('skilljar-content').classList.remove('grey-bg-d05')
+
+Array.from(document.querySelectorAll('fa-carret-down'))
+  .forEach((item) => {
+    item.classList.remove('fa-carret-down')
+    item.classList.add('fa-angle-down')
+  })
+
+if (!document.getElementById('catalog-filter-menu')) {
+  document.getElementById('catalog-left-nav').classList.add('d-none')
+  document.getElementById('skilljar-content').classList.add('no-margin')
+}
