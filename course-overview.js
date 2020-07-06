659689f1bab4e3d53c5ad0bcf75f4383d49e4c2c
Etienne Burdet <etienne.burdet@gmail.com>
2020-07-06 17:13:02 +0200

    add sj autosubscription

--- course-overview.js
+++ course-overview.js
@@ -8,3 +8,14 @@ if (summary) {
     '<img src="https://etienneburdet.com/ods-academy/empty-datasets_yellow.svg" class="course-image">'
   )
 }
+
+//SJ autosubscription
+$(document).ready(function () {
+    var refGuides = document.querySelectorAll('#skilljar-content #catalog-content #catalog-courses a[data-tags*="auto-register"]')
+    refGuides.forEach(function (node) {
+        var href = node.href;
+        if (!~href.indexOf('?reg=1')) {
+            node.href = href + '?reg=1';
+        }
+    })
+})
