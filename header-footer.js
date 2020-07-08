bc3838cf42aeaae653e00542a6ceb9e029f8ae9b
Etienne Burdet <etienne.burdet@gmail.com>
2020-07-08 15:38:33 +0200

    refacto header

--- header-footer.js
+++ header-footer.js
@@ -1,61 +1,92 @@
+/*global sessionStorage */
+
 const headerLeft =   document.getElementById('header-left')
 const headerRight = document.getElementById('header-right')
 const langButton = document.querySelector('.header-lang')
-
-const toggleChoice = () => {
-  langButton.classList.toggle('current-lang')
-  langButton.classList.toggle('choice')
-}
-
-!sessionStorage.getItem('lang') && sessionStorage.setItem('lang','fr')
-
-if (sessionStorage.getItem('lang') === 'fr') {
-  headerLeft.insertAdjacentHTML('beforeend',`
-    <a href="/page/catalogue-cours" class="header-link">Tous les cours</a>
-    <a href="/page/parcours-thematiques" class="header-link">Parcours thématiques</a>
-    <a href="/page/bibliotheque-des-savoirs" class="header-link">Bibliothèque des savoirs</a>
-  `)
-  headerRight.insertAdjacentHTML('afterbegin',`
-    <div>
-      <details class="header-lang-btn button">
-        <summary>Français</summary>
-        <span id="lang-choice">English</span>
-      </details>
-    </div>
-  `)
-
-  document.getElementById('lang-choice')
-    .addEventListener('click', ev => {
-      sessionStorage.setItem('lang', 'en')
-      window.location.href = '/page/homepage'
-    })
-} else {
-  headerLeft.insertAdjacentHTML('beforeend',`
-    <a href="/page/all-courses" class="header-link">All courses</a>
-    <a href="/page/thematic-courses" class="header-link">Thematic courses</a>
-    <a href="/page/knowledge-library" class="header-link">Knowledge library</a>
-  `)
-  headerRight.insertAdjacentHTML('afterbegin',`
-    <div>
-      <details class="header-lang-btn button">
-        <summary>English</summary>
-        <span id="lang-choice">Français</span>
-      </details>
-    <div>
-  `)
-
-  document.getElementById('lang-choice')
-    .addEventListener('click', ev => {
-      sessionStorage.setItem('lang', 'fr')
-      window.location.href = '/'
-    })
-}
-
 const footerLeft = document.getElementById('footer-left')
-footerLeft.insertAdjacentHTML('beforeend',`
-  <a class="footer-content" href="https://legal.opendatasoft.com/fr/privacy-policy.html" target="_blank">Politique de confidentialité</a>
-  <a class="footer-ontent" href="https://legal.opendatasoft.com/fr/cookies.html" target="_blank">Cookies</a>
-`)
+
+const headerLinks = {
+  fr: [
+    {
+      name: "Tous les cours",
+      url: "https://opendatasoft.skilljar.com/page/catalogue-cours"
+    },
+    {
+      name: "Parcours thématiques",
+      url: "https://opendatasoft.skilljar.com/page/parcours-thematiques"
+    },
+    {
+      name: "Bibliothèque des savoirs"
+    }
+  ],
+  en: [
+    {
+      name: "All courses",
+      url: "https://opendatasoft.skilljar.com/page/all-courses"
+    },
+    {
+      name: "Thematic courses",
+      url: "https://opendatasoft.skilljar.com/page/thematic-courses"
+    },
+    {
+      name: "Knowledge library",
+      url: "https://opendatasoft.skilljar.com/page/knowledge-library"
+    }
+  ]
+}
+
+
+const insertHeaderLinks = (lang) => {
+  headerLinks[lang].forEach(link => headerLeft.insertAdjacentHTML('beforeend',
+    `<a href="${link.url}" class="header-link">${link.name}</a>`
+  ))
+}
+
+const insertFooterLinks = (lang) => {
+  footerLeft.insertAdjacentHTML('beforeend',/*html*/`
+    <a class="footer-content" href="https://legal.opendatasoft.com/${lang}/privacy-policy.html" target="_blank">
+      ${lang === 'fr' ? 'Politique de confidentialité' : 'Privacy Policy'}
+    </a>
+    <a class="footer-ontent" href="https://legal.opendatasoft.com/${lang}/cookies.html" target="_blank">Cookies</a>
+  `)
+}
+
+const insertLangButton = (lang) => {
+  headerRight.insertAdjacentHTML('afterbegin',/*html*/`
+    <div>
+      <details class="header-lang-btn button">
+        <summary>${lang.toUpperCase()}</summary>
+        <span id="lang-choice">${ lang === 'fr' ? 'EN' : 'FR'}</span>
+      </details>
+    <div>
+  `)
+}
+
+const renderLinks = (lang) => {
+  insertLangButton(lang)
+  insertFooterLinks(lang)
+  insertHeaderLinks(lang)
+}
+
+const toggleLang = () => {
+  const lang = sessionStorage.getItem('lang')
+  if (lang === 'fr') {
+    sessionStorage.setItem('lang', 'en')
+    window.location.href = 'https://opendatasoft.skilljar.com/page/homepage'
+  } else {
+    sessionStorage.setItem('lang', 'fr')
+    window.location.href = 'https://opendatasoft.skilljar.com/'
+  }
+}
+
+const initLangSelect = () => {
+  !sessionStorage.getItem('lang') && sessionStorage.setItem('lang','fr')
+  renderLinks(sessionStorage.getItem('lang'))
+  const langChoice = document.getElementById('lang-choice')
+  langChoice.addEventListener('click', toggleLang)
+}
+
+initLangSelect()
 
 // //sj scripts
 // $('#ep-footer').append('<div class=footer-content> <a href="https://legal.opendatasoft.com/fr/privacy-policy.html" target="_blank">Politique de confidentialité</a></div>');
