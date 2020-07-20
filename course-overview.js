ebbd83aa4b133eefd210e78236a2125312c1cf38
Etienne Burdet <etienne.burdet@gmail.com>
2020-07-20 15:17:23 +0200

    V0 launch!

--- course-overview.js
+++ course-overview.js
@@ -1,14 +1,6 @@
 Array.from(document.querySelectorAll('section'))
   .forEach((section) => section.classList.add('active'))
 
-// const summary = document.querySelector('.sj-summary')
-// if (summary) {
-//   summary.insertAdjacentHTML(
-//     'beforebegin',
-//     '<img src="https://etienneburdet.com/ods-academy/empty-datasets_yellow.svg" class="course-image">'
-//   )
-// }
-
 //SJ autosubscription
 $(document).ready(function () {
     var refGuides = document.querySelectorAll('#skilljar-content #catalog-content #catalog-courses a[data-tags*="auto-register"]')
